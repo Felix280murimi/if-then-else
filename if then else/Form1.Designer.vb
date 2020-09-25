1f8348517448f3e81de9f71c705cdb50bbb5b6e8
Felix murimi <61313608+Felix280murimi@users.noreply.github.com>
2020-09-25 12:53:01 +0300

    Add project files.

---
+++ if then else/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDayOfTheWeek = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnDayOfTheWeek
+        '
+        Me.btnDayOfTheWeek.Location = New System.Drawing.Point(350, 370)
+        Me.btnDayOfTheWeek.Name = "btnDayOfTheWeek"
+        Me.btnDayOfTheWeek.Size = New System.Drawing.Size(259, 23)
+        Me.btnDayOfTheWeek.TabIndex = 0
+        Me.btnDayOfTheWeek.Text = "siku Gani Hii?"
+        Me.btnDayOfTheWeek.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnDayOfTheWeek)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnDayOfTheWeek As Button
+End Class
---
+++ if then else/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub btnDayOfTheWeek_Click(sender As Object, e As EventArgs) Handles btnDayOfTheWeek.Click
+        Dim today As Integer
+        today = Val(InputBox("Enter an interger in the range from 1 to 7"))
+        If (today = 5) Then
+            MessageBox.Show("Leo ni Furahi Day .MEMBERS DAY")
+        ElseIf (today = 3) Then
+            MessageBox.Show("Leo ni wednesday.LADIES NIGHT to Night")
+        ElseIf (today = 7) Then
+            MessageBox.Show("Leo ni Sunday.Praise and Glory To God")
+        ElseIf (today > 7) Then
+            MessageBox.Show("INVALID ENTRY")
+        End If
+        MessageBox.Show("Life Continues")
+    End Sub
+End Class
